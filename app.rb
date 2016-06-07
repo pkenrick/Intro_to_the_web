91263eaf41026c638bd8f73bc1c2d4403fb65ef7
paulkenrick <pkenrick@gmail.com>
2016-06-07 07:50:04 +0100

    Server now responding with picture

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "hello!"
 end
 
 get '/secret' do
   "this is the secret page... :)"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  <div>"
 end
 
 
